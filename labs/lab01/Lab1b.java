914e63e02d6fe39f5c8b9da3cf6d6a8dd92f661d
Charlie Francesconi <m251806@usna.edu>
2023-01-07 19:30:19 -0500

    Move function calls in Lab1

--- labs/lab01/Lab1b.java
+++ labs/lab01/Lab1b.java
@@ -1,5 +1,19 @@
 import java.util.*;
 public class Lab1b {
+    public static int factorial(int k) {
+        if(k == 0) {
+            return 1;
+        }
+        return k*factorial(k-1);
+    }
+    
+    public static int numerator(int n, int k) {
+        if(k == 1) {
+            return n;
+        }
+        return (n-k+1) * numerator(n,k-1);
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter your name: ");
@@ -15,19 +29,5 @@ public class Lab1b {
         System.out.println("denominator = " + denominator);
         System.out.println("odds = 1 in " + numerator/denominator + " = " + (double)1/(numerator/denominator));
         System.out.println("Goodbye " + name + ".");
-    }
-
-    public static int numerator(int n, int k) {
-        if(k == 1) {
-            return n;
-        }
-        return (n-k+1) * numerator(n,k-1);
-    }
-
-    public static int factorial(int k) {
-        if(k == 0) {
-            return 1;
-        }
-        return k*factorial(k-1);
     }
 }
